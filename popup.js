87d78b66406e450cd2cb1816a721da9486152e5d
Tim <tim_tran@live.ca>
2020-01-26 15:11:23 -0800

    updating

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
-// setup = function() {
+console.log("JS loaded")
 
-// }
-// $(document).ready(setup)
-console.log("testing")
+channelList = []
+
+displayInput = function(value) {
+	// var name = document.getElementById("channelName");
+	channelList.push(value);
+
+	var listLength = channelList.length
+
+	console.log(listLength)
+
+}
+// $(document).ready(displayInput)
+// console.log("testing")
